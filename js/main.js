9f5e65dd003a118bb4d11fa72dd6290affa27f62
koiblue2800 <cmsoowordpress@gmail.com>
2025-12-08 15:19:53 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", () => {
+  if (!window.RufflePlayer) {
+    console.error("Ruffle no se cargó correctamente.");
+    return;
+  }
+
+  const ruffle = window.RufflePlayer.newest();
+  const container = document.getElementById("game-container");
+  const player = ruffle.createPlayer();
+
+  player.config = {
+    autoplay: "on",
+    unmuteOverlay: "hidden",
+    splashScreen: false
+  };
+
+  container.appendChild(player);
+  player.load("assets/");
+});
+
+
